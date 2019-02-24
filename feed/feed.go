6fc4a9635212758f10c2428ab9a09e548d2524e8
Naoki Oketani <okepy.naoki@gmail.com>
2019-02-24 19:20:52 +0900

    add comment

--- feed/feed.go
+++ feed/feed.go
@@ -2,6 +2,7 @@ package feed
 
 import "strings"
 
+// ExtractUsnTitle is a function to get USN-XXXX-X string.
 func ExtractUsnTitle(s string) string {
 	tmp := strings.Replace(s, "<![CDATA[", "", -1)
 	return strings.Split(tmp, ":")[0]
